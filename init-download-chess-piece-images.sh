af3c1295419bc873d549ec2f9def016ee1533f7d
alex@hawryschuk.com <alex@hawryschuk.com>
2025-08-02 17:33:38 +0800

    init: chess board application

---
+++ init-download-chess-piece-images.sh
@@ -0,0 +1,15 @@
+mkdir -p img/chesspieces/wikipedia
+cd img/chesspieces/wikipedia
+
+wget https://chessboardjs.com/img/chesspieces/wikipedia/wP.png
+wget https://chessboardjs.com/img/chesspieces/wikipedia/bP.png
+wget https://chessboardjs.com/img/chesspieces/wikipedia/wR.png
+wget https://chessboardjs.com/img/chesspieces/wikipedia/bR.png
+wget https://chessboardjs.com/img/chesspieces/wikipedia/wN.png
+wget https://chessboardjs.com/img/chesspieces/wikipedia/bN.png
+wget https://chessboardjs.com/img/chesspieces/wikipedia/wB.png
+wget https://chessboardjs.com/img/chesspieces/wikipedia/bB.png
+wget https://chessboardjs.com/img/chesspieces/wikipedia/wQ.png
+wget https://chessboardjs.com/img/chesspieces/wikipedia/bQ.png
+wget https://chessboardjs.com/img/chesspieces/wikipedia/wK.png
+wget https://chessboardjs.com/img/chesspieces/wikipedia/bK.png
